0c1d32a724dbe60bf6607050d98f42f629052363
rickcontreras <ricardo.contreras568@hotmail.com>
2025-06-08 22:19:38 +0000

    🏗️ feat(swagger): Añadir configuración de Swagger para documentación de API

---
+++ src/main/java/com/telconova/tracking/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.telconova.tracking.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+        @Value("${spring.application.name}")
+        private String applicationName;
+
+        @Value("${server.port:8080}")
+        private String serverPort;
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .addServersItem(new Server().url("http://localhost:" + serverPort))
+                                .components(new Components().addSecuritySchemes("bearerAuth",
+                                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                                                .scheme("bearer")
+                                                                .bearerFormat("JWT")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .info(new Info().title(
+                                                "API de Tracking Service - TelcoNova SupportSuite")
+                                                .description("Microservicio para gestión de avances técnicos")
+                                                .version("1.0.0")
+                                                .contact(new Contact().name("Equipo de TelcoNova")
+                                                                .email("dev@telconova.com")
+                                                                .url("https://telconova.com"))
+                                                .license(new License().name("Licencia Privada").url(
+                                                                "https://telconova.com/licencia")));
+        }
+}
